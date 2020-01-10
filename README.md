24874bfe8a066410a02dec368020812de17818e3
Toby <tdudleyoffice@gmail.com>
2020-01-10 11:28:13 +0000

    add readme and a user story to functional_tests.py. Set up NewVisitorTest

---
+++ README.md
@@ -0,0 +1,16 @@
+## Dani's Game
+
+The need for Dani's game arose on 4th January 2020 where my extended family couldn't all play "Danni's game", named after my cousin Danni, at the same time without one person needing to be quiz master.
+
+I solved this with a simple python app on the day using my phone but I have decided to create a project using TDD and Django to make the solution available to my cousin and her family any time.
+
+#### Is Django the best tool for the job?
+Absolutely not. It is complete overkill but I wanted to use the TDD methodology to build a simple Django app from first principles, and this seemed like the perfect project in terms of scope. The methodology used in this is heavily inspired by Harry Percival's excellent book: Test-Driven Development with Python.
+
+#### How does the game work?
+* The game is played in a group. Each player submits a name of a person or fictional character to the quiz master.
+* The quiz master then reads all the names out once, and only once.
+* The youngest player then begins by addressing any one other player and says are you ___ ?
+* If they are correct, the other player joins the youngest player's team and they question another person.
+* If they are incorrect, the chance to ask a question moves to the player to whom the question was posed, and they ask another player a question.
+* The game continues until everyone is one one big team.
--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,47 @@
 from selenium import webdriver
+import unittest
 
 browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert "Django" in browser.title
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_game_and_retrieve_it_later(self):
+
+        # Dani wants to play her guess who game with her family without the
+        # need for a quiz master. She has heard that she can use a cool new
+        # website, she goes to the home page
+        self.browser.get("http://localhost:8000")
+
+        # She notices that the game is named after her
+        self.assertIn("Danni", self.browser.title)
+        self.fail("Finish the test")
+
+        # She is invited to dive straight in and add player 1's name
+        # She types 'Henry the Hoover' into a text box
+
+        # When she hits enter, the page updates, and now the page shows
+        # player 1 with a confirmation next to it that they she has played
+        # and player 2 appears beneath with a text box next to it
+
+        # She passes to her brother, who enters 'Pingu' into the textbox and
+        # hits enter
+
+        # The page updates again, showing player 1 and player 2 have entered
+        # their player names and the game is now waiting for player 3
+
+        # Danni wonders whether the site will remember the game, because her
+        # dad, the third player is busy at the moment. She notices the site has
+        # generated a unique URL for her which she can revisit at any time.
+
+        # She visits the URL on her phone and notices the game is still there,
+        # waiting for player 3's input.
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
